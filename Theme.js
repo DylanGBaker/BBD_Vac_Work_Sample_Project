6e202660b5f08fed97b96980722d99c1626f44df
Dylan Baker <skaduvee32@gmail.com>
2023-12-04 13:02:24 +0200

    Update Theme.js

--- Theme.js
+++ Theme.js
@@ -5,13 +5,21 @@ const theme_button = document.querySelector(".theme-button");
 
 let current_theme = "dark";
 
+if (localStorage.getItem("theme") != "")
+{
+  current_theme = localStorage.getItem("theme");
+}
+
 function changeToDarkMode() {
   page_background.style.backgroundColor = "black";
   for (let i = 0; i < calculator_buttons.length; i++) {
     calculator_buttons[i].style.color = "white";
     calculator_buttons[i].style.background = "none";
     calculator_buttons[i].style.backgroundColor = "black";
+    calculator_buttons[i].style.border = "solid";
+    calculator_buttons[i].style.borderColor = "orange";
   }
+  localStorage.setItem("theme", "dark");
   current_theme = "light";
 }
 
@@ -21,7 +29,9 @@ function changeToLightTheme() {
     calculator_buttons[i].style.color = "white";
     calculator_buttons[i].style.backgroundImage =
       "linear-gradient(160deg, rgba(41, 185, 214, 0.6) 80%, #ffffff 100%)";
+      calculator_buttons[i].style.border = "none";
   }
+  localStorage.setItem("theme", "light");
   current_theme = "dark";
 }
 
